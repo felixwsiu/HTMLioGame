b8c950ebac4d879bff016c4f2d6d7b31606418f8
felixwsiu <46659537+felixwsiu@users.noreply.github.com>
2019-02-21 15:38:48 -0800

    Created Login Form
    - login form, and sign in connected to MongoDB Atlas
    - authentication to prevent direct loading of pages
    -encryption of passwords on database
    -bootstrap, ejs styling

--- Login/app.js
+++ Login/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
-// DB Config
-const db = require('./config/keys').MongoURI
 
+// Passport config
+
+require('./config/passport')(passport);
+
+
+// DB Config
+const db = require('./config/keys').MongoURI;
 
 // Connect to Mongo
 mongoose.connect(db,{ useNewUrlParser : true})
@@ -18,10 +26,31 @@ mongoose.connect(db,{ useNewUrlParser : true})
 app.use(expressLayouts);
 app.set('view engine','ejs');
 
-
 // Bodyparser
 app.use(express.urlencoded({ extended: false }));
 
+// Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+
+// Passport 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global Vars for Messages
+app.use((req,res,next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 //Routes
 app.use('/',require('./routes/index'));
--- Login/routes/users.js
+++ Login/routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs')
+const passport = require('passport');
+// User model
+const User = require('../models/User');
 
 // Login Page
 router.get('/login',(req,res) => res.render('login'));
@@ -38,8 +41,59 @@ router.post('/register', (req,res) => {
             password2
         });
     } else {
-        res.send('pass');
+        User.findOne({email : email})
+            .then(user => {
+                if (user){ //User exists
+                    errors.push({msg: 'Email is already registered'});
+                    res.render('register',{
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+
+                    // Hash Password
+                    bcrypt.genSalt(10,(err,salt) => 
+                        bcrypt.hash(newUser.password,salt, (err,hash) => {
+                            if(err) throw err;
+                            newUser.password = hash; //Password is now hashed
+                            //Save user
+                            newUser.save()
+                                .then(user =>{
+                                    req.flash('success_msg', ' You are now registered and can log in');
+                                    res.redirect('/users/login');
+                                })
+                                .catch(err => console.log(err));
+                        } ))
+                }
+
+            });
     }
+});
+
+//Login Handle
+router.post('/login',(req,res,next) => {
+    passport.authenticate('local', {
+        successRedirect: '/game',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req,res,next);
+
+});
+
+
+//Logout Handle
+router.get('/logout',(req,res) => {
+    req.logout();
+    req.flash('success_msg','You have successfully logged out');
+    res.redirect('/users/login');
 });
 
 module.exports = router;
