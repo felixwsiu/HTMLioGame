1533e20b7a13087e0144585c1cb673ec53c2780a
felixwsiu <46659537+felixwsiu@users.noreply.github.com>
2019-02-21 03:44:22 -0800

    Set up connection with MongoDB Atlas with Node.js

---
+++ Login/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI:'mongodb://felix123:felix123@htmlio-shard-00-00-tab9m.mongodb.net:27017,htmlio-shard-00-01-tab9m.mongodb.net:27017,htmlio-shard-00-02-tab9m.mongodb.net:27017/test?ssl=true&replicaSet=HTMLIO-shard-0&authSource=admin&retryWrites=true'
+}
